7882fd35ef84ee0d3b73a7def3a15c42b7bb91f6
bawornpak <bawanpak.bou@gmail.com>
2017-02-12 10:22:24 +0700

    gg

---
+++ app/src/main/java/com/example/kamon/icareadhd/MyConstant.java
@@ -0,0 +1,15 @@
+package com.example.kamon.icareadhd;
+
+/**
+ * Created by bawornpak on 2/12/2017.
+ */
+
+public class MyConstant {
+    //explicit
+
+    private String[] LoginButtonMain = new String[]{"login","เข้าสู่ระบบ"};
+
+    public String[] getLoginButtonMain() {
+        return LoginButtonMain;
+    }
+}// main
