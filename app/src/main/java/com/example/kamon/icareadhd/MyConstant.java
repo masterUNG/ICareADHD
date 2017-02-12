96dbac2b0605396b6aa12b9d8f2f2d1cbccf1aaa
Kamonmew <kamontuang@gmail.com>
2017-02-12 11:21:14 +0700

    Textok

--- app/src/main/java/com/example/kamon/icareadhd/MyConstant.java
+++ app/src/main/java/com/example/kamon/icareadhd/MyConstant.java
@@ -12,12 +12,20 @@ public class MyConstant {
     private String[] EmailButtonMain = new String[] {"Email Address","ชื่ออีเมลล์"};
     private String[] PasswordButtonMain = new String[] {"Password","รหัสผ่าน"};
 
+    private String[] UserTypeButtonRegister= new String[] {"User Type","ประเภทผู้ใช้"};
     private String[] CreatingButtonRegister= new String[] {"Creating Account","สร้างบัญชีผู้ใช้"};
     private String[] FirstNameButtonRegister= new String[] {"First Name","ชื่อจิง"};
     private String[] LastNameButtonRegister= new String[] {"Last Name","นามสกุล"};
     private String[] EmailButtonRegister= new String[] {"Email","อีเมลล์"};
     private String[] PasswordButtonRegister= new String[] {"Password","รหัสผ่าน"};
     private String[] DoneButtonRegister= new String[] {"Done","เสร็จสิ้น"};
+
+    private String[] WelcomeButtonHome= new String[] {"Welcome to iCare-ADHD","ขอต้อนรับเข้าสู่ iCare-ADHD"};
+    private String[] AgendaButtonHome= new String[] {"Agenda","กำหนดการต่างๆ"};
+    private String[] ListOfMedicationButtonHome= new String[] {"List of Medication","ตารางการบริโภคยา"};
+    private String[] ListOfTaskButtonHome= new String[] {"List of Task","ตารางกิจกรรม"};
+    private String[] DateButtonHome= new String[] {"Date","วัน"};
+    private String[] AppoinmentButtonHome= new String[] {"Appointment","กำหนดการนัดหมาายพบแพทย์"};
 
 
 
@@ -42,8 +50,16 @@ public class MyConstant {
         return PasswordButtonMain;
     }
 
+
+
+
+
     public String[] getCreatingButtonRegister() {
         return CreatingButtonRegister;
+    }
+
+    public String[] getUserTypeButtonRegister() {
+        return UserTypeButtonRegister;
     }
 
     public String[] getFirstNameButtonRegister() {
@@ -64,5 +80,30 @@ public class MyConstant {
 
     public String[] getDoneButtonRegister() {
         return DoneButtonRegister;
+    }
+
+
+    public String[] getWelcomeButtonHome() {
+        return WelcomeButtonHome;
+    }
+
+    public String[] getAgendaButtonHome() {
+        return AgendaButtonHome;
+    }
+
+    public String[] getListOfMedicationButtonHome() {
+        return ListOfMedicationButtonHome;
+    }
+
+    public String[] getListOfTaskButtonHome() {
+        return ListOfTaskButtonHome;
+    }
+
+    public String[] getDateButtonHome() {
+        return DateButtonHome;
+    }
+
+    public String[] getAppoinmentButtonHome() {
+        return AppoinmentButtonHome;
     }
 }// main
